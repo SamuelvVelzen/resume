e48fabdd88ca20eeb9689d4ab718ad51242f90d5
Samuel <samuelvvelzen@icloud.com>
2024-08-09 16:38:19 +0200

    implement nav

--- client/src/app/pages/homepage/homepage.component.ts
+++ client/src/app/pages/homepage/homepage.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { NavItemComponent } from '../../ui/nav/nav-item/nav-item.component';
 import { NavbarComponent } from '../../ui/nav/navbar/navbar.component';
 
 @Component({
   selector: 'svv-homepage',
   standalone: true,
-  imports: [NavbarComponent],
+  imports: [NavbarComponent, NavItemComponent],
   templateUrl: 'homepage.component.html',
   styleUrls: ['homepage.component.scss'],
 })
