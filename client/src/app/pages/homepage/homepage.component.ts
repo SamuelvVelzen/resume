a4136e76e8b5e5c679a7152b9aa4444e0251f66b
Samuel <samuelvvelzen@icloud.com>
2024-08-09 11:47:36 +0200

    change selector

---
+++ client/src/app/pages/homepage/homepage.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'svv-homepage',
+  templateUrl: 'homepage.component.html',
+  styleUrls: ['homepage.component.scss'],
+})
+export class HomepageComponent {
+  constructor() {}
+}
