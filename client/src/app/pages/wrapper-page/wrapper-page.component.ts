b03e1aaa2d286f1063949d1182601d180a453661
Samuel <samuelvvelzen@icloud.com>
2024-08-11 00:21:26 +0200

    implement links

--- client/src/app/pages/wrapper-page/wrapper-page.component.ts
+++ client/src/app/pages/wrapper-page/wrapper-page.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LinkComponent } from '../../ui/link/link/link.component';
 import { NavItemComponent } from '../../ui/nav/nav-item/nav-item.component';
 import { NavbarComponent } from '../../ui/nav/navbar/navbar.component';
 
 @Component({
   selector: 'svv-wrapper-page',
-  imports: [NavbarComponent, NavItemComponent, RouterOutlet],
+  imports: [NavbarComponent, NavItemComponent, RouterOutlet, LinkComponent],
   templateUrl: './wrapper-page.component.html',
   styleUrls: ['./wrapper-page.component.scss'],
   standalone: true,
