3416ff0daa06bffc1f2342d7dd62a2cbaa3becd1
Samuel <samuelvvelzen@icloud.com>
2024-08-10 22:53:14 +0200

    implement new icons

--- client/src/app/pages/homepage/homepage.component.ts
+++ client/src/app/pages/homepage/homepage.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { IconPlacementComponent } from '../../ui/icon/icon-placement/icon-placement.component';
 import { NavItemComponent } from '../../ui/nav/nav-item/nav-item.component';
 import { NavbarComponent } from '../../ui/nav/navbar/navbar.component';
 
 @Component({
   selector: 'svv-homepage',
   standalone: true,
-  imports: [NavbarComponent, NavItemComponent, RouterOutlet],
+  imports: [
+    NavbarComponent,
+    NavItemComponent,
+    RouterOutlet,
+    IconPlacementComponent,
+  ],
   templateUrl: 'homepage.component.html',
   styleUrls: ['homepage.component.scss'],
 })
