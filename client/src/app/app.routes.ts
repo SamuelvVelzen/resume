6a88d4b710f9db0a20aee07bf55d3fdd048bdd27
Samuel <samuelvvelzen@icloud.com>
2024-08-10 21:59:19 +0200

    add back the homepage with the routes

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,29 +1,31 @@
 import { Routes } from '@angular/router';
+import { HomepageComponent } from './pages/homepage/homepage.component';
 import { LandingPageComponent } from './pages/landing-page/landing-page.component';
 import { ROUTES_SEGMENTS } from './pages/routes-segments';
+import { WrapperPageComponent } from './pages/wrapper-page/wrapper-page.component';
 
 export const routes: Routes = [
   {
     path: ROUTES_SEGMENTS.root,
     component: LandingPageComponent,
   },
-  // {
-  //   path: ROUTES_SEGMENTS.resume,
-  //   component: WrapperPageComponent,
-  //   children: [
-  //     {
-  //       path: '',
-  //       pathMatch: 'full',
-  //       redirectTo: ROUTES_SEGMENTS.home,
-  //     },
-  //     {
-  //       path: ROUTES_SEGMENTS.home,
-  //       component: HomepageComponent,
-  //     },
-  //     {
-  //       path: ROUTES_SEGMENTS.aboutMe,
-  //       component: AboutMePageComponent,
-  //     },
-  //   ],
-  // },
+  {
+    path: ROUTES_SEGMENTS.resume,
+    component: WrapperPageComponent,
+    children: [
+      {
+        path: '',
+        pathMatch: 'full',
+        redirectTo: ROUTES_SEGMENTS.home,
+      },
+      {
+        path: ROUTES_SEGMENTS.home,
+        component: HomepageComponent,
+      },
+      // {
+      //   path: ROUTES_SEGMENTS.aboutMe,
+      //   component: AboutMePageComponent,
+      // },
+    ],
+  },
 ];
