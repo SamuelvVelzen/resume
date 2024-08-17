048bb5fbb4a3df4c56a19aa36c4f7ef5be5cd226
Samuel <samuelvvelzen@icloud.com>
2024-08-18 01:11:37 +0200

    improve folder for libs

---
+++ client/src/app/libs/common/ui/index.ts
@@ -0,0 +1,4 @@
+export * from './icon/icon-placement/icon-placement.component';
+export * from './link/link.component';
+export * from './nav/nav-item/nav-item.component';
+export * from './nav/navbar/navbar.component';
