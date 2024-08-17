df1ad5dddb8dfba6d39665dccc8efe407dfdf9fe
Samuel <samuelvvelzen@icloud.com>
2024-08-18 01:11:37 +0200

    improve folder for libs

--- client/src/app/libs/common/ui/link/link.component.ts
+++ client/src/app/libs/common/ui/link/link.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input } from '@angular/core';
-import { IconPlacementComponent } from '../../icon/icon-placement/icon-placement.component';
+import { IconPlacementComponent } from '../icon/icon-placement/icon-placement.component';
 
 @Component({
   selector: 'svv-link',
---
+++ client/src/app/libs/common/util/index.ts
@@ -0,0 +1,1 @@
+export * from './browser-title/browser-title.service';
