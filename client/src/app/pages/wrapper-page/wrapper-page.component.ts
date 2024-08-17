2ff285c9464c228df4c5e0b522c9a0a1e12b314b
Samuel <samuelvvelzen@icloud.com>
2024-08-18 01:11:59 +0200

    fix imports

--- client/src/app/pages/wrapper-page/wrapper-page.component.ts
+++ client/src/app/pages/wrapper-page/wrapper-page.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { LinkComponent } from '@SVV/ui/link/link/link.component';
-import { NavItemComponent } from '@SVV/ui/nav/nav-item/nav-item.component';
-import { NavbarComponent } from '@SVV/ui/nav/navbar/navbar.component';
+import {
+  LinkComponent,
+  NavbarComponent,
+  NavItemComponent,
+} from '@SVV/common/ui';
 
 @Component({
   selector: 'svv-wrapper-page',
