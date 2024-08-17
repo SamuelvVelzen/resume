1734dc7d5d2adeed66a62f3c8a18bae4c6929ff3
Samuel <samuelvvelzen@icloud.com>
2024-08-18 01:35:38 +0200

    start implementing on project page

--- client/src/app/pages/project-page/project-page.component.ts
+++ client/src/app/pages/project-page/project-page.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { ProjectComponent } from '@SVV/projects';
 
 @Component({
   selector: 'svv-project-page',
   templateUrl: './project-page.component.html',
   styleUrls: ['./project-page.component.scss'],
   standalone: true,
+  imports: [ProjectComponent],
 })
 export class ProjectPageComponent {}
