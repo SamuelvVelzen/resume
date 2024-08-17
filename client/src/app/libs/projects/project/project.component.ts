28e534bfdd254e09a215f6e2857dbc245771de47
Samuel <samuelvvelzen@icloud.com>
2024-08-18 01:33:12 +0200

    create project component

---
+++ client/src/app/libs/projects/project/project.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { BadgeComponent } from '@SVV/common/ui/badge/badge.component';
+
+@Component({
+  selector: 'svv-project',
+  templateUrl: './project.component.html',
+  styleUrls: ['./project.component.scss'],
+  standalone: true,
+  providers: [BadgeComponent],
+  imports: [BadgeComponent],
+})
+export class ProjectComponent {}
