a58918f65ecb10fb8057c5a36bb3add137937e78
Samuel <samuelvvelzen@icloud.com>
2024-08-18 00:57:04 +0200

    create project page

---
+++ client/src/app/pages/project-page/project-page.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'svv-project-page',
+  templateUrl: './project-page.component.html',
+  styleUrls: ['./project-page.component.scss'],
+  standalone: true,
+})
+export class ProjectPageComponent {}
