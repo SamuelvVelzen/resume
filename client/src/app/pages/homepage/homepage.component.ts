b8024732e9eb489aa5d6b0c682cc4ec5684d76d4
Samuel <samuelvvelzen@icloud.com>
2025-08-23 21:15:23 +0200

    add skill badge to homepage

--- client/src/app/pages/homepage/homepage.component.ts
+++ client/src/app/pages/homepage/homepage.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SkillBadgeComponent } from '@SVV/ui/badge/skill-badge/skill-badge.component';
 import { IconPlacementComponent } from '@SVV/ui/icon/icon-placement/icon-placement.component';
 import { NavItemComponent } from '@SVV/ui/nav/nav-item/nav-item.component';
 import { NavbarComponent } from '@SVV/ui/nav/navbar/navbar.component';
@@ -12,6 +13,7 @@ import { NavbarComponent } from '@SVV/ui/nav/navbar/navbar.component';
     NavItemComponent,
     RouterOutlet,
     IconPlacementComponent,
+    SkillBadgeComponent,
   ],
   templateUrl: 'homepage.component.html',
   styleUrls: ['homepage.component.scss'],
